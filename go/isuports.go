043828599909754ce3ef37100cd998cdec7ad70e
Ryu Yamada <a2sin2a2ko@kyoto-y.work>
2022-07-23 13:28:12 +0900

    fix desc

--- go/isuports.go
+++ go/isuports.go
@@ -1373,7 +1373,7 @@ func competitionRankingHandler(c echo.Context) error {
 	if err := tenantDB.SelectContext(
 		ctx,
 		&pss,
-		"SELECT tenant_id, id, player_id, competition_id, score, max(row_num) as row_num, created_at, updated_at FROM player_score WHERE tenant_id = ? AND competition_id = ? GROUP BY player_id ORDER BY score, row_num",
+		"SELECT tenant_id, id, player_id, competition_id, score, max(row_num) as row_num, created_at, updated_at FROM player_score WHERE tenant_id = ? AND competition_id = ? GROUP BY player_id ORDER BY score DESC, row_num",
 		tenant.ID,
 		competitionID,
 	); err != nil {
