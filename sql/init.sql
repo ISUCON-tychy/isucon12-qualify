f5bd3014caac05a8e0d79c1a02a02aa1caa0dc81
yokonao <yokotukanao@me.com>
2022-07-23 12:11:00 +0900

    fix table name

--- sql/init.sql
+++ sql/init.sql
@@ -3,5 +3,5 @@ DELETE FROM visit_history WHERE created_at >= '1654041600';
 UPDATE id_generator SET id=2678400000 WHERE stub='a';
 ALTER TABLE id_generator AUTO_INCREMENT=2678400000;
 
-ALTER TABLE posts DROP INDEX `tenant_id_idx` ;
-ALTER TABLE posts ADD INDEX `tenant_id_idx` (`tenant_id`, `competition_id`, `player_id`);
+ALTER TABLE visit_history DROP INDEX `tenant_id_idx` ;
+ALTER TABLE visit_history ADD INDEX `tenant_id_idx` (`tenant_id`, `competition_id`, `player_id`);
